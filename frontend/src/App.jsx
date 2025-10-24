11b126e6eb6ece8b8f91750be145517b72ee0c7f
felkor0 <felixkorshavn@gmail.com>
2025-10-24 10:37:49 +0200

    added functionality to update products

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,8 +5,14 @@ function App() {
 	const [sections, setSections] = useState([]);
 	const [products, setProducts] = useState([]);
 	const [name, setName] = useState("");
+	const [sectionName, setSectionName] = useState("");
 	const [section, setSection] = useState(""); // section_id
+	const [reloadSections, setReloadSections] = useState(false);
 	const [error, setError] = useState(null);
+	const [isAddingSection, setIsAddingSection] = useState(false);
+	const [isUpdatingProductVisible, setIsUpdatingProductVisible] = useState(false);
+	const [updatedName, setUpdatedName] = useState("");
+	const [selectedProductId, setSelectedProductId] = useState(null);
 
 	// Hämta sektioner
 	useEffect(() => {
@@ -14,7 +20,7 @@ function App() {
 			.then((res) => res.json())
 			.then((data) => setSections(Array.isArray(data) ? data : []))
 			.catch((err) => setError(err.message));
-	}, []);
+	}, [reloadSections]);
 
 	const removeProduct = async (id) => {
 		try {
@@ -27,6 +33,24 @@ function App() {
 			if (!res.ok) throw new Error("Kunde inte ta bort produkt");
 
 			getProducts(); // uppdatera listan efter borttagning
+		} catch (err) {
+			setError(err.message);
+		}
+	};
+
+	const updateProduct = async () => {
+		try {
+			const res = await fetch("/api/products", {
+				method: "PUT",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({ id: selectedProductId, name: updatedName }),
+			});
+
+			if (!res.ok) throw new Error("Kunde inte uppdatera produkt");
+			setIsUpdatingProductVisible(false);
+			setUpdatedName("");
+			setSelectedProductId(null);
+			getProducts(); // uppdatera listan efter uppdatering
 		} catch (err) {
 			setError(err.message);
 		}
@@ -69,12 +93,36 @@ function App() {
 		}
 	};
 
+	const handleAddSection = async () => {
+		if (!sectionName) {
+			setError("Fyll i avdelning");
+			return;
+		}
+
+		try {
+			console.log("Adding section:", sectionName);
+			const res = await fetch("/api/sections", {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({ name: sectionName }),
+			});
+
+			if (!res.ok) throw new Error("Kunde inte lägga till avdelning");
+
+			setIsAddingSection(false);
+			setSectionName("");
+			setError(null);
+			setReloadSections((prev) => !prev); // uppdatera sektioner
+		} catch (err) {
+			setError(err.message);
+		}
+	};
+
 	return (
 		<div className="container">
 			<div className="formcard-container">
 				<h1 className="heading">Lägg till i lista</h1>
 				{error && <p style={{ color: "red" }}>Fel: {error}</p>}
-
 				<form
 					onSubmit={(e) => {
 						e.preventDefault();
@@ -91,7 +139,6 @@ function App() {
 								className="text-input-form"
 							/>
 						</label>
-
 						<label>
 							Avdelning:
 							<select
@@ -107,13 +154,36 @@ function App() {
 							</select>
 						</label>
 					</div>
-
-					<button type="submit" className="submit-btn">
-						Lägg till
-					</button>
+					<div className="btn-div">
+						<button type="submit" className="submit-btn">
+							Lägg till
+						</button>
+					</div>
 				</form>
+				<button className="add-btn" onClick={() => setIsAddingSection(true)}>
+					Lägg till avdelning
+				</button>
+				{isAddingSection && (
+					<div className="container-div" onClick={() => setIsAddingSection(false)}>
+						<div className="holder-div" onClick={(e) => e.stopPropagation()}>
+							<h1 className="heading">Lägg till avdelning</h1>
+							<div className="input-group-section" onClick={(e) => e.stopPropagation()}>
+								<input
+									value={sectionName}
+									onChange={(e) => setSectionName(e.target.value)}
+									placeholder="Avdelning"
+									type="text"
+									className="text-input-form"
+									onClick={(e) => e.stopPropagation()}
+								/>
+								<button type="button" className="addbtn" onClick={() => handleAddSection()}>
+									Lägg till
+								</button>
+							</div>
+						</div>
+					</div>
+				)}
 			</div>
-
 			<div className="card">
 				<h1 className="heading">Att handla</h1>
 				<ul className="section-list">
@@ -126,10 +196,39 @@ function App() {
 								<button className="btn-remove" onClick={() => removeProduct(item.id)}>
 									Ta bort
 								</button>
+								<button
+									onClick={() => {
+										setSelectedProductId(item.id);
+										setUpdatedName(item.name);
+										setIsUpdatingProductVisible(true);
+									}}
+									className="btn-remove">
+									Uppdatera
+								</button>
 							</li>
 						))
 					)}
 				</ul>
+				{isUpdatingProductVisible && (
+					<div className="container-div" onClick={() => setIsUpdatingProductVisible(false)}>
+						<div className="holder-div" onClick={(e) => e.stopPropagation()}>
+							<h1 className="heading">Uppdatera produkt</h1>
+							<div className="input-group-section" onClick={(e) => e.stopPropagation()}>
+								<input
+									value={updatedName}
+									onChange={(e) => setUpdatedName(e.target.value)}
+									placeholder="Ny produktnamn"
+									type="text"
+									className="text-input-form"
+									onClick={(e) => e.stopPropagation()}
+								/>
+								<button type="button" className="addbtn" onClick={updateProduct}>
+									Uppdatera
+								</button>
+							</div>
+						</div>
+					</div>
+				)}
 			</div>
 		</div>
 	);
